13ac69ccf7923ae420ef6fbef0217ee8b6316141
Gergely Kucsera <kucseragergely1@gmail.com>
2019-12-01 19:10:19 +0100

    update2

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -13,12 +13,26 @@ namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
+
         public Form1()
         {
             InitializeComponent();
-            lblLastName.Text = Resource1.LastName; // label1
-            lblFirstName.Text = Resource1.FirstName; // label2
+            lblLastName.Text = Resource1.FullName; // label1
             btnAdd.Text = Resource1.Add; // button1
+
+            listUsers.DataSource = users;
+            //listUsers.ValueMember = "ID";
+            listUsers.DisplayMember = "FullName";
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                FullName = fullname.Text
+            };
+            users.Add(u);
         }
     }
 }
